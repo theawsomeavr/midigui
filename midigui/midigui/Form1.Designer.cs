1ca057eaefe7234fd90e18e72f3f6512d43e67f2
theawsomeavr <32213630+theawsomeavr@users.noreply.github.com>
2018-12-21 12:04:40 -0700

    Add files via upload

---
+++ midigui/midigui/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class midigui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.channel = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(180, 48);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(58, 17);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "use -t3";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(180, 84);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(58, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "use -t4";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 169);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "go";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // channel
+            // 
+            this.channel.AutoSize = true;
+            this.channel.Location = new System.Drawing.Point(53, 219);
+            this.channel.Name = "channel";
+            this.channel.Size = new System.Drawing.Size(159, 17);
+            this.channel.TabIndex = 5;
+            this.channel.Text = "use percussion (channel 10)";
+            this.channel.UseVisualStyleBackColor = true;
+            this.channel.CheckedChanged += new System.EventHandler(this.channel_CheckedChanged);
+            // 
+            // midigui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.channel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "midigui";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox channel;
+    }
+}
+
---
+++ midigui/midigui/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+namespace WindowsFormsApplication1
+{
+    public partial class midigui : Form
+    {
+        public midigui()
+        {
+           
+            InitializeComponent();
+        }
+        OpenFileDialog file = new OpenFileDialog();
+        string result;
+        bool t3;
+        bool t4;
+        bool chan;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            file.Filter = "mid|*.mid";
+             if(file.ShowDialog()==DialogResult.OK){
+                 string filename = file.FileName;
+                 string extension = System.IO.Path.GetExtension(filename);
+            result = filename.Substring(0, filename.Length - extension.Length);
+            textBox1.Text = file.SafeFileName;
+             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string c;
+            string arg=" ";
+        if (t3 == true) arg = "-t3 " ;
+       
+        if (t4 == true) arg = "-t4 " ;
+        
+        if (t4 == false && t3 == false) arg = " ";
+        if (chan == true) c = " ";
+        else c = "-pi ";
+               ProcessStartInfo startInfo = new ProcessStartInfo();
+               startInfo.FileName = "miditones.exe";
+               startInfo.Arguments = c+arg+result;
+               Process.Start(startInfo);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            t3 = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            t4 = checkBox2.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void channel_CheckedChanged(object sender, EventArgs e)
+        {
+            chan = channel.Checked;
+        }
+    }
+}
